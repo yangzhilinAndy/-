3d4a5ca69c14a4a326ca515f6b11d8eea3e7b85a
yangzhilinAndy <yy_2067@qq.com>
2018-11-03 08:48:04 +0800

    Create 03_11_2018-32-Longest Valid Parentheses.cpp

---
+++ 03_11_2018-32-Longest Valid Parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if (s.length()==0)
+            return 0;
+        int dp[50000]={0}, maxlen=0;
+        for (int i=1; i<s.length(); i++)
+        {
+            if (s[i]=='(')
+                dp[i]=0;
+            else if (s[i-1]=='(')
+            {
+                if (i-2>=0)
+                    dp[i]=dp[i-2]+2;
+                else
+                    dp[i]=2;
+            }
+            else if (i-dp[i-1]-1<0)
+                dp[i]=0;
+            else if (s[i-dp[i-1]-1]=='(')
+            {
+                if (i-dp[i-1]-2>=0)
+                    dp[i]=2+dp[i-1]+dp[i-dp[i-1]-2]; 
+                else
+                    dp[i]=2+dp[i-1];
+            }
+        }
+        for (int i=1; i<s.length(); i++)
+            if (dp[i]>maxlen)
+                maxlen=dp[i];
+        return maxlen;
+    }
+};
