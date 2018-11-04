49a0514150bf26a5c3eadc9cf5b0310791b3f34a
yangzhilinAndy <yy_2067@qq.com>
2018-11-04 19:47:00 +0800

    Create 04_11_2018-14-Longest Common Prefix.cpp

---
+++ 04_11_2018-14-Longest Common Prefix.cpp
@@ -0,0 +1,40 @@
+//approach 1: divide and conquer
+
+class Solution {
+public:
+    string findCommonPrefix(string str1, string str2)
+    {
+        int cnt=0;
+        int len=(str1.length()<str2.length())?str1.length():str2.length();
+        for (cnt=0;cnt<len;cnt++)
+            if (str1[cnt]!=str2[cnt])
+                break;
+        string pref(str1, 0, cnt);
+        return pref;  
+    }
+    
+    string PrefixInRange(int beg, int end, vector<string>& strs)
+    {
+        string prefix,left,right;
+        int mid=(beg+end)/2;
+        if (beg==end)
+            return strs[beg];
+        if (beg+1==end)
+            return findCommonPrefix(strs[beg], strs[end]);
+        left=PrefixInRange(beg, mid, strs);
+        right=PrefixInRange(mid+1, end, strs);
+        prefix=findCommonPrefix(left, right);
+        return prefix;
+    }
+    
+    string longestCommonPrefix(vector<string>& strs) {
+     int size=strs.size();
+     if (size==0)
+         return "";
+     for (string str : strs)
+        if (str.length()==0)
+            return "";
+      string prefix=PrefixInRange(0, size-1, strs);
+      return prefix;
+    }
+};
